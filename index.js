e24746acd8cd2e02a76384346099d9b411521963
Spartex <vedantgarode11@gmail.com>
2023-10-30 17:48:30 +0530

    Done

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// script.js
 const outerElements = document.querySelectorAll('.outer');
 
 outerElements.forEach((outer) => {
@@ -7,12 +6,20 @@ outerElements.forEach((outer) => {
 
     function changeGradient() {
         const yellowHue = 60; // Hue for yellow
-        const redHue = 0;    // Hue for red
-
         const color1 = `hsla(${yellowHue + gradientPercentage}, 100%, 50%, 0.7)`; // Yellow to red
         const color2 = `hsla(${yellowHue + gradientPercentage + 60}, 100%, 50%, 0.7)`; // Yellow to red
         const gradient = `linear-gradient(to right, ${color1}, ${color2})`;
+
+        // Set the background image to the gradient
         outer.style.backgroundImage = gradient;
+
+        // Calculate the box-shadow color dynamically
+        const glowColor = `hsla(${yellowHue + gradientPercentage}, 100%, 50%, 0.7)`;
+        outer.style.boxShadow = `0 0 20px ${glowColor}`;
+
+        // Apply blur filter using a CSS rule
+        const blurFilterRule = `backdrop-filter: blur(50px);`; // Adjust the blur amount as needed
+        outer.style.cssText += blurFilterRule;
 
         gradientPercentage += direction;
 
@@ -24,6 +31,8 @@ outerElements.forEach((outer) => {
 
     setInterval(changeGradient, 20); // Change gradient every 20 milliseconds for each div
 });
+
+
 
 
 const menuToggle = document.getElementById('menu-toggle');
@@ -50,3 +59,20 @@ menuLinks.forEach((link) => {
     });
 });
 
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            const offset = targetElement.getBoundingClientRect().top - (window.innerHeight / 5);
+            window.scrollTo({
+                top: window.scrollY + offset,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
